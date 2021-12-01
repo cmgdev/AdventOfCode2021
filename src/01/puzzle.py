1074dfd8c44c831b2b36e418a796c5ebe2b98e02
cmgdev <854806+cmgdev@users.noreply.github.com>
2021-11-30 23:50:40 -0600

    day 1 part 2

--- src/01/puzzle.py
+++ src/01/puzzle.py
@@ -1,6 +1,18 @@
 from pathlib import Path
 import itertools
 
+def getNumIncreases(input):
+    current = 0
+    previous = 1000
+    increases = 0
+
+    for i in input:
+        current = int(i)
+        if current > previous:
+            increases = increases + 1
+        previous = current
+    return increases
+
 IS_TEST = False
 
 inputFile = "example.txt" if IS_TEST else "input.txt"
@@ -10,19 +22,31 @@ input = list(
     filter(lambda l: not l.startswith("answer") and not l.startswith("#"),
            fileContents))
 
-expected = int(list(
+expected1 = int(list(
     filter(lambda l: l.startswith("answer1:"), fileContents))
     [0].removeprefix("answer1:"))
 
-current = 0
-previous = 1000
-increases = 0
-
-for i in input:
-    current = int(i)
-    if current > previous:
-        increases = increases + 1
-    previous = current
+increases = getNumIncreases(input)
     
 print(increases)
-print(increases == expected)
+print(increases == expected1)
+
+
+
+expected2 = int(list(
+    filter(lambda l: l.startswith("answer2:"), fileContents))
+    [0].removeprefix("answer2:"))
+
+windows = []
+
+idx = 0
+
+while idx + 2 < len(input):
+    windows.append(int(input[idx]) + int(input[idx + 1]) + int(input[idx + 2]))
+    idx = idx + 1
+    
+# print(windows)
+increases = getNumIncreases(windows)
+print(increases)
+print(increases == expected2)
+    
