e415fb480f5b779c81df6e1cea75979398ed0429
cmgdev <854806+cmgdev@users.noreply.github.com>
2021-12-05 00:59:31 -0600

    day 5 part 2

--- src/05/Puzzle05.java
+++ src/05/Puzzle05.java
@@ -18,51 +18,81 @@ public class Puzzle05 {
             String expected2 = lines.stream().filter(l -> l.startsWith("answer2:")).map(l -> l.replace("answer2:", ""))
                     .findFirst().get();
 
-            List<Vent> vents = lines.stream().filter(l -> !l.startsWith("answer"))
-                    .map(l -> {
-                        String start = l.split(" -> ")[0];
-                        String end = l.split(" -> ")[1];
-                        String[] startPoint = start.split(",");
-                        String[] endPoint = end.split(",");
-                        return new Vent(
-                                new Point(Integer.parseInt(startPoint[0]), Integer.parseInt(startPoint[1])),
-                                new Point(Integer.parseInt(endPoint[0]), Integer.parseInt(endPoint[1])));
-                    })
-                    .filter(v -> v.a.x == v.b.x || v.a.y == v.b.y)
-                    .toList();
-            // System.out.println(vents);
-            
-            Map<Point, Integer> pointCounts = new HashMap<>();
-            for( Vent vent : vents){
-                if( vent.a.x != vent.b.x){
-                    int start = Math.min(vent.a.x, vent.b.x);
-                    int end = Math.max(vent.a.x, vent.b.x);
-                    for(int i = start; i <= end; i++){
-                        int y = vent.a.y;
-                        Point p = new Point(i, y);
-                        int count = pointCounts.getOrDefault(p, 0) + 1;
-                        pointCounts.put(p, count);
-                    }
-                }
-                else if( vent.a.y != vent.b.y){
-                    int start = Math.min(vent.a.y, vent.b.y);
-                    int end = Math.max(vent.a.y, vent.b.y);
-                    for(int i = start; i <= end; i++){
-                        int x = vent.a.x;
-                        Point p = new Point(x, i);
-                        int count = pointCounts.getOrDefault(p, 0) + 1;
-                        pointCounts.put(p, count);
-                    }
-                }
-            }
+            List<Vent> vents = getVents(lines);
+            Map<Point, Integer> pointCounts = getPointCounts(vents, false);
             // System.out.println(pointCounts);
             long multi = pointCounts.values().stream().filter(c -> c > 1).count();
             System.out.println(multi);
             System.out.println(multi == Long.parseLong(expected1));
 
+            pointCounts = getPointCounts(vents, true);
+            // System.out.println(pointCounts);
+            multi = pointCounts.values().stream().filter(c -> c > 1).count();
+            System.out.println(multi);
+            System.out.println(multi == Long.parseLong(expected2));
+
         } catch (Exception e) {
             System.out.println("Oh shit! " + e);
         }
+    }
+
+    private static Map<Point, Integer> getPointCounts(List<Vent> vents, boolean includeDiagonals) {
+        Map<Point, Integer> pointCounts = new HashMap<>();
+        for (Vent vent : vents) {
+            if( !includeDiagonals && !(vent.a.x == vent.b.x || vent.a.y == vent.b.y)){
+                continue;
+            }
+
+            if (vent.a.x != vent.b.x && vent.a.y == vent.b.y) {
+                int start = Math.min(vent.a.x, vent.b.x);
+                int end = Math.max(vent.a.x, vent.b.x);
+                for (int i = start; i <= end; i++) {
+                    int y = vent.a.y;
+                    Point p = new Point(i, y);
+                    int count = pointCounts.getOrDefault(p, 0) + 1;
+                    pointCounts.put(p, count);
+                }
+            } else if (vent.a.y != vent.b.y && vent.a.x == vent.b.x) {
+                int start = Math.min(vent.a.y, vent.b.y);
+                int end = Math.max(vent.a.y, vent.b.y);
+                for (int i = start; i <= end; i++) {
+                    int x = vent.a.x;
+                    Point p = new Point(x, i);
+                    int count = pointCounts.getOrDefault(p, 0) + 1;
+                    pointCounts.put(p, count);
+                }
+            }
+            else if(includeDiagonals){
+                int deltaX = (vent.b.x - vent.a.x) > 0 ? 1 : -1;
+                int deltaY = (vent.b.y - vent.a.y) > 0 ? 1 : -1;
+                int currX = vent.a.x;
+                int currY = vent.a.y;
+
+                while(currX != vent.b.x + deltaX && currY != vent.b.y + deltaY){
+                    Point p = new Point(currX, currY);
+                    int count = pointCounts.getOrDefault(p, 0) + 1;
+                    pointCounts.put(p, count);
+                    currX += deltaX;
+                    currY += deltaY;
+                }
+            }
+        }
+        return pointCounts;
+    }
+
+    private static List<Vent> getVents(List<String> lines) {
+        List<Vent> vents = lines.stream().filter(l -> !l.startsWith("answer"))
+                .map(l -> {
+                    String start = l.split(" -> ")[0];
+                    String end = l.split(" -> ")[1];
+                    String[] startPoint = start.split(",");
+                    String[] endPoint = end.split(",");
+                    return new Vent(
+                            new Point(Integer.parseInt(startPoint[0]), Integer.parseInt(startPoint[1])),
+                            new Point(Integer.parseInt(endPoint[0]), Integer.parseInt(endPoint[1])));
+                })
+                .toList();
+        return vents;
     }
 
     record Point(int x, int y) {
