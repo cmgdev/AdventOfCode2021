8405aaf3549a7f71013ec0e2d6295343ba03cdd0
cmgdev <854806+cmgdev@users.noreply.github.com>
2021-12-05 23:53:16 -0600

    day 6 part 1

--- src/06/Puzzle06.java
+++ src/06/Puzzle06.java
@@ -1,9 +1,10 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Puzzle06 {
-    final static boolean IS_TEST = true;
+    final static boolean IS_TEST = false;
     static Path input = Path.of("./src/06/" + (IS_TEST ? "example.txt" : "input.txt"));
 
     public static void main(String... args) {
@@ -14,8 +15,41 @@ public class Puzzle06 {
                     .findFirst().get();
             String expected2 = lines.stream().filter(l -> l.startsWith("answer2:")).map(l -> l.replace("answer2:", ""))
                     .findFirst().get();
+
+            int currentDay = 0;
+            int maxDays = 80;
+            List<LanternFish> lanternFish = new ArrayList<>();
+            for (String f : lines.get(0).split(",")) {
+                lanternFish.add(new LanternFish(Integer.parseInt(f)));
+            }
+
+            while (currentDay < maxDays) {
+                System.out.println("Day " + currentDay + ": " + lanternFish.size());
+                List<LanternFish> newFish = new ArrayList<>();
+
+                for (LanternFish fish : lanternFish) {
+                    if (fish.age == 0) {
+                        newFish.add(new LanternFish(6)); // reset fish
+                        newFish.add(new LanternFish(8)); // new fish
+                    } else {
+                        newFish.add(new LanternFish(fish.age - 1) );
+                    }
+                }
+
+                lanternFish = newFish;
+                currentDay++;
+            }
+            System.out.println(lanternFish.size());
+            System.out.println(lanternFish.size() == Integer.parseInt(expected1));
+
         } catch (Exception e) {
             System.out.println("Shit! " + e);
         }
+    }
+
+    /**
+     * LanternFish
+     */
+    public record LanternFish(int age) {
     }
 }
