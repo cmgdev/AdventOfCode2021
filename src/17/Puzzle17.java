6310dd1cf8db503c12fa0e8d445ebf0e248dedd5
cmgdev <854806+cmgdev@users.noreply.github.com>
2021-12-18 17:25:54 -0600

    day 17 part 1

--- src/17/Puzzle17.java
+++ src/17/Puzzle17.java
@@ -3,7 +3,7 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class Puzzle17 {
-    final static boolean IS_TEST = true;
+    final static boolean IS_TEST = false;
     static Path input = Path.of("./src/17/" + (IS_TEST ? "example.txt" : "input.txt"));
 
     public static void main(String... args) {
@@ -14,8 +14,67 @@ public class Puzzle17 {
                     .findFirst().get();
             String expected2 = lines.stream().filter(l -> l.startsWith("answer2:")).map(l -> l.replace("answer2:", ""))
                     .findFirst().get();
+
+            String[] tokens = lines.get(0).split(",");
+            String xs = tokens[0].replace("x=", "");
+            int minX = Integer.parseInt(xs.split("\\.\\.")[0]);
+            int maxX = Integer.parseInt(xs.split("\\.\\.")[1]);
+
+            String ys = tokens[1].replace("y=", "");
+            int minY = Integer.parseInt(ys.split("\\.\\.")[0]);
+            int maxY = Integer.parseInt(ys.split("\\.\\.")[1]);
+
+            TargetArea t = new TargetArea(minX, maxX, minY, maxY);
+
+            int highestY = 0;
+            for (int initXv = 0; initXv < 200; initXv++) {
+                for (int initYv = 0; initYv < 200; initYv++) {
+                    // System.out.println("running " + initXv + "," + initYv);
+                    int probeX = 0;
+                    int probeY = 0;
+                    int xv = initXv;
+                    int yv = initYv;
+                    int thisHighestY = probeY;
+
+                    while (!(t.isInTargetArea(probeX, probeY) || t.isPastTargetArea(probeX, probeY))) {
+                        probeX += xv;
+                        probeY += yv;
+                        thisHighestY = Math.max(thisHighestY, probeY);
+                        if (xv > 0) {
+                            xv--;
+                        } else if (xv < 0) {
+                            xv++;
+                        }
+                        yv--;
+                    }
+                    if (t.isInTargetArea(probeX, probeY) && thisHighestY > highestY) {
+                        highestY = Math.max(highestY, thisHighestY);
+                        System.out.println("highestY is now " + highestY);
+                        System.out.println("init xv,yv is " + initXv + "," + initYv);
+                    }
+                    // System.out.println(t.isInTargetArea(probeX, probeY));
+                    // System.out.println(t.isPastTargetArea(probeX, probeY));
+                    // if (t.isPastTargetArea(probeX, probeY)) {
+                    //     break;
+                    // }
+                }
+            }
+
+            System.out.println("highestY is " + highestY);
+            System.out.println(highestY == Integer.parseInt(expected1));
+
         } catch (Exception e) {
             System.out.println("Shit! " + e);
+        }
+    }
+
+    record TargetArea(int minX, int maxX, int minY, int maxY) {
+        boolean isInTargetArea(int x, int y) {
+            return x >= minX && x <= maxX && y >= minY && y <= maxY;
+        }
+
+        boolean isPastTargetArea(int x, int y) {
+            return x > maxX || y < minY;
         }
     }
 }
