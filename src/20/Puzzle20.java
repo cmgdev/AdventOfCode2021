dfe92a07aa4b0e25be3ecab5cd8c8993e0edb0ec
cmgdev <854806+cmgdev@users.noreply.github.com>
2021-12-20 12:54:03 -0600

    day 20 part 2

--- src/20/Puzzle20.java
+++ src/20/Puzzle20.java
@@ -34,31 +34,39 @@ public class Puzzle20 {
                 }
             }
 
-            int rounds = 2;
-            image = expand(image, rounds);
-            int[] minMax = getMinMax(image);
-            for (int r = 0; r < rounds; r++) {
-                Map<Pixel, String> newImage = new HashMap<>();
-                int margin = rounds - r;
-                for (int x = minMax[MIN_X] - margin; x <= minMax[MAX_X] + margin; x++) {
-                    for (int y = minMax[MIN_Y] - margin; y <= minMax[MAX_Y] + margin; y++) {
-                        Pixel p = new Pixel(x, y);
-                        int idx = p.getBinFromNeighbors(image);
-                        String next = enhancementAlgorithm.substring(idx, idx + 1);
-                        newImage.put(p, next);
-                    }
-                }
-                image = newImage;
-                System.out.println("Round " + r);
-                // printImage(image);
-            }
+            image = enhance(enhancementAlgorithm, image, 2);
             long lightPixels = image.values().stream().filter(v -> v.equals("#")).count();
             System.out.println(lightPixels);
             System.out.println(lightPixels == Long.parseLong(expected1));
 
+            image = enhance(enhancementAlgorithm, image, 48);
+            lightPixels = image.values().stream().filter(v -> v.equals("#")).count();
+            System.out.println(lightPixels);
+            System.out.println(lightPixels == Long.parseLong(expected2));
         } catch (Exception e) {
             System.out.println("Shit! " + e);
         }
+    }
+
+    private static Map<Pixel, String> enhance(String enhancementAlgorithm, Map<Pixel, String> image, int rounds) {
+        image = expand(image, rounds);
+        int[] minMax = getMinMax(image);
+        for (int r = 0; r < rounds; r++) {
+            Map<Pixel, String> newImage = new HashMap<>();
+            int margin = rounds - r;
+            for (int x = minMax[MIN_X] - margin; x <= minMax[MAX_X] + margin; x++) {
+                for (int y = minMax[MIN_Y] - margin; y <= minMax[MAX_Y] + margin; y++) {
+                    Pixel p = new Pixel(x, y);
+                    int idx = p.getBinFromNeighbors(image);
+                    String next = enhancementAlgorithm.substring(idx, idx + 1);
+                    newImage.put(p, next);
+                }
+            }
+            image = newImage;
+            System.out.println("Round " + r);
+            // printImage(image);
+        }
+        return image;
     }
 
     static Map<Pixel, String> expand(Map<Pixel, String> image, int amt) {
