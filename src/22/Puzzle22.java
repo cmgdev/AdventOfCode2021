a434d8ce3426f5a2c259774c93a888ccc0a0c89f
cmgdev <854806+cmgdev@users.noreply.github.com>
2021-12-24 11:02:50 -0500

    in progress

--- src/22/Puzzle22.java
+++ src/22/Puzzle22.java
@@ -1,6 +1,7 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -30,16 +31,18 @@ public class Puzzle22 {
 
             lines.removeIf(l -> l.startsWith("answer") || l.startsWith("#"));
             List<Instruction> instructions = new ArrayList<>();
+            int id = 1;
             for (var line : lines) {
                 String onOff = line.split(" ")[0];
                 String[] coords = line.split(" ")[1].split(",");
                 String[] xs = coords[0].split("=")[1].split("\\.\\.");
                 String[] ys = coords[1].split("=")[1].split("\\.\\.");
                 String[] zs = coords[2].split("=")[1].split("\\.\\.");
-                instructions.add(new Instruction(onOff,
+                instructions.add(new Instruction(onOff, false,
                         Integer.parseInt(xs[0]), Integer.parseInt(xs[1]),
                         Integer.parseInt(ys[0]), Integer.parseInt(ys[1]),
-                        Integer.parseInt(zs[0]), Integer.parseInt(zs[1])));
+                        Integer.parseInt(zs[0]), Integer.parseInt(zs[1]),
+                        Integer.toString(id++)));
             }
 
             Map<Point, Boolean> cube = new HashMap<>();
@@ -57,28 +60,50 @@ public class Puzzle22 {
             System.out.println(on);
             System.out.println(on == Long.parseLong(expected1));
 
-            Set<Instruction> allInstructions = new HashSet<>();
-            for (int i = 0; i < instructions.size() - 1; i++) {
-                Instruction a = instructions.get(i);
-                allInstructions.add(a);
-                for (int j = i + 1; j < instructions.size(); j++) {
+            LinkedList<Instruction> instrQueue = new LinkedList<>(instructions);
+            Map<Boolean, Set<Instruction>> allInstructions = new HashMap<>();
+            allInstructions.put(true, new HashSet<>());
+            allInstructions.put(false, new HashSet<>());
+
+            while (!instrQueue.isEmpty()) {
+                Instruction a = instrQueue.pop();
+                allInstructions.get(a.isOn()).add(a);
+                for (int j = 0; j < instructions.size(); j++) {
                     Instruction b = instructions.get(j);
-                    allInstructions.add(b);
                     Instruction intersect = a.getIntersection(b);
-                    if (intersect != null) {
-                        allInstructions.add(intersect);
+                    if (intersect != null && !instrQueue.contains(intersect)
+                            && !allInstructions.get(intersect.isOn()).contains(intersect)) {
+                        instrQueue.addLast(intersect);
                     }
                 }
             }
-            
-            long numOn = 0;
-            for(var instruction : allInstructions){
-                if(instruction.isOn()){
-                    numOn += instruction.getVolume();
+
+            for (var instructionSet : allInstructions.values()) {
+                for (var instr : instructionSet) {
+                    System.out.println(instr);
                 }
             }
 
+            // long sumOnNoIntersect = allInstructions.get(true).stream().filter(i ->
+            // !i.isIntersection()).mapToLong(i -> i.getVolume()).sum();
+            // long sumOffNoIntersect = allInstructions.get(false).stream().filter(i ->
+            // !i.isIntersection()).mapToLong(i -> i.getVolume()).sum();
+            // long sumOnIntersect = allInstructions.get(true).stream().filter(i ->
+            // i.isIntersection()).mapToLong(i -> i.getVolume()).sum();
+            // long sumOffIntersect = allInstructions.get(false).stream().filter(i ->
+            // i.isIntersection()).mapToLong(i -> i.getVolume()).sum();
 
+            // System.out.println(sumOnNoIntersect);
+            // System.out.println(sumOffNoIntersect);
+            // System.out.println(sumOnIntersect);
+            // System.out.println(sumOffIntersect);
+
+            // long numOn = 0;
+            // for(var instruction : allInstructions){
+            // if(instruction.isOn()){
+            // numOn += instruction.getVolume();
+            // }
+            // }
 
         } catch (Exception e) {
             System.out.println("Shit! " + e);
@@ -88,7 +113,8 @@ public class Puzzle22 {
     record Point(int x, int y, int z) {
     }
 
-    record Instruction(String onOff, int minX, int maxX, int minY, int maxY, int minZ, int maxZ) {
+    record Instruction(String onOff, boolean isIntersection, int minX, int maxX, int minY, int maxY, int minZ,
+            int maxZ, String id) {
         boolean isOn() {
             return onOff.equals("on");
         }
@@ -97,7 +123,7 @@ public class Puzzle22 {
             long x = Math.abs(Integer.valueOf(maxX).longValue() - Integer.valueOf(minX).longValue());
             long y = Math.abs(Integer.valueOf(maxY).longValue() - Integer.valueOf(minY).longValue());
             long z = Math.abs(Integer.valueOf(maxZ).longValue() - Integer.valueOf(minZ).longValue());
-            System.out.println(x + " * " + y + " * " + z);
+            // System.out.println(x + " * " + y + " * " + z);
             return x * y * z;
         }
 
@@ -146,9 +172,22 @@ public class Puzzle22 {
                 int minMaxZ = Math.min(this.maxZ(), other.maxZ());
                 int maxMinZ = Math.max(this.minZ(), other.minZ());
                 String onOff = this.isOn() && other.isOn() ? "on" : "off";
-                return new Instruction(onOff, maxMinX, minMaxX, maxMinY, minMaxY, maxMinZ, minMaxZ);
+                return new Instruction(onOff, true, maxMinX, minMaxX, maxMinY, minMaxY, maxMinZ, minMaxZ,
+                        this.id + "." + other.id);
             }
             return null;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Instruction other = (Instruction) obj;
+            return this.isOn() == other.isOn()
+                    && this.maxX == other.maxX
+                    && this.minX == other.minX
+                    && this.maxY == other.maxY
+                    && this.minY == other.minY
+                    && this.maxZ == other.maxZ
+                    && this.minZ == other.minZ;
         }
 
         // List<Instruction> getIntersections(List<Instruction> others) {
@@ -171,4 +210,5 @@ public class Puzzle22 {
     static boolean isBetween(int a, int min, int max) {
         return a >= min && a <= max;
     }
+
 }
